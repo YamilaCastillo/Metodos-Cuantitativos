df932899b0b8b0fc375a1130a1c750bbebd3e9a6
adisleidys <adysol803@gmail.com>
2020-09-21 22:54:34 -0500

    regresion lineal

--- Tarea 2-21.09.20/tarea 2.R
+++ Tarea 2-21.09.20/tarea 2.R
@@ -1,5 +1,40 @@
-df=read.csv("D:/MAESTRIA NEE/GITHUB/Tarea 2-21.09.20/presion.csv")
+df=read.csv("C:/+++++++++++++++/maestria/2do Semestre/alvaro/tarea2/Metodos-Cuantitativos/Tarea 2-21.09.20/presion.csv")
 df
 x=df$Edades
 y=df$Presion
 plot(x,y)
+
+
+y =mx+n
+
+n=y-mx
+
+m =dy/dx
+
+#recta de regresion lineal 
+m=cov(y,x)/var(x)
+
+n=mean(y)-m*mean(x)
+
+m;n
+plot(x,y)
+abline(n,m,col="red")
+
+#predec
+resy=m*30+n
+resy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
